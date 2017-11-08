54af38962fe9f55190271702a8255a5f940b5d3d
hanjonc <33489234+hanjonc@users.noreply.github.com>
2017-11-08 22:34:52 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        public static int Fib(int x)
+        {
+            int num = 0;
+            int num2 = 1;
+            int fibonacci = 0;
+
+            if (x == 0)
+            {
+                return 0;
+            }
+
+            else if (x == 1)
+            {
+                return 1;
+            }
+
+
+            else
+            {
+                for (int i = 1; i <= x; i++)
+                {
+                    fibonacci = num + num2;
+                    num = num2;
+                    num2 = fibonacci;
+                }
+                return fibonacci;
+            }
+            
+        }
+        public static void Main(string[] args)
+        {
+            int fibo = 0;
+            int even = 0;
+
+            for (int i = 1; i < 33; i++)
+            {
+                fibo = Fib(i);
+                if (fibo % 2 == 0)
+                {
+                    even += fibo;
+                }
+                
+                Console.Write("{0} ", fibo);
+                
+
+            }
+            Console.Write("\n\nSum of even-valued terms: " + even);
+            Console.ReadKey();
+        }
+    }
+
+}
